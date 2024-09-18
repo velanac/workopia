0da69012ae7fdaeaef28ac0ea0614d518b33c2cc
velanac <vidmedia@gmail.com>
2024-09-18 23:01:07 +0200

    Database connection

--- public/index.php
+++ public/index.php
@@ -1,17 +1,16 @@
 <?php
 require_once '../helpers.php';
+require_once basePath('Database.php');
+require_once basePath('Router.php');
+$config = require_once basePath('config/db.php');
 
-$routes = [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
+$db = new Database($config);
+
+$router = new Router();
+
+$routes = require basePath('routes.php');
 
 $uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-if (array_key_exists($uri, $routes)) {
-    require(basePath($routes[$uri]));
-} else {
-    require basePath($routes['404']);
-}
+$router->route($uri, $method);
