f4777b864063cd68605b64c92f65d32254a63ff7
velanac <vidmedia@gmail.com>
2024-09-19 13:11:14 +0200

    Error controller

--- Framework/Router.php
+++ Framework/Router.php
@@ -2,6 +2,8 @@
 
 namespace Framework;
 
+use App\Controllers\ErrorController;
+
 class Router
 {
     protected $routes = [];
@@ -75,19 +77,6 @@ class Router
     }
 
     /**
-     * Load error page
-     * @param int $httpCode
-     * 
-     * @return void
-     */
-    public function error($httpCode = 404)
-    {
-        http_response_code($httpCode);
-        loadView("error/{$httpCode}");
-        exit;
-    }
-
-    /**
      * Route the request
      * 
      * @param string $uri
@@ -106,6 +95,6 @@ class Router
             }
         }
 
-        $this->error();
+        ErrorController::notFound();
     }
 }
